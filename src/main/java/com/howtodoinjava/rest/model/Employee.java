256f196ca877dfb219f54a8ef00fad22a697af8e
Naveen Prapulla SADDALA <naveen.saddala@amadeus.com>
2020-02-19 22:14:33 +0530

    first commit

---
+++ src/main/java/com/howtodoinjava/rest/model/Employee.java
@@ -0,0 +1,35 @@
+package com.howtodoinjava.rest.model;
+ 
+public class Employee {
+ 
+    public Employee() {
+ 
+    }
+ 
+    public Employee(Integer id, String firstName, String lastName, String email) {
+        super();
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+  
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String email;
+ 
+    //Getters and setters
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	
+ 
+    @Override
+    public String toString() {
+        return "Employee [id=" + id + ", firstName=" + firstName + ", lastName=" + lastName + ", email=" + email + "]";
+    }
+}
